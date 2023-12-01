b28ad8f4dfecc76ed1981835bb5fc1eb0f4302e9
hashtag-squirrel <alita2000@web.de>
2023-12-01 15:35:44 +0100

    Refactoring day1

--- day01/solution.py
+++ day01/solution.py
@@ -1,10 +1,11 @@
 # Read file and split into lines
-file = open('day01/input.txt', 'r')
+file = open('day01/example.txt', 'r')
 lines = file.readlines()
 
 # declare count as 0
 count = 0
 
+# list of number words
 words = [
     'one',
     'two',
@@ -17,6 +18,7 @@ words = [
     'nine'
 ]
 
+# list of number words containing the number
 replacements = [
     'o1e',
     't2o',
@@ -31,17 +33,18 @@ replacements = [
 
 # iterate over lines
 for line in lines:
+    # replace number words with replacements
     for i in range(len(words)):
         if words[i] in line:
             line = line.replace(words[i], replacements[i])
 
-    all_numbers = []
-    for char in line:
-        if char.isnumeric():
-            all_numbers.append(char)
-    print(all_numbers)
+    # add numbers in line to list
+    all_numbers = [char for char in line if char.isnumeric()]
+
+    # concatenate first and last number in list
     number_to_add = all_numbers[0] + all_numbers[-1]
 
+    # add number to total count
     count += int(number_to_add)
 
 # print total count
